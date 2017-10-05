12a86d4931a9b66af09a95e223d248b9e0eafde2
Shaojin Ding <Shaojin Ding>
2017-10-05 17:13:22 -0500

    basic layout

---
+++ HelloWorld/app/controllers/student_controller.rb
@@ -0,0 +1,8 @@
+class StudentController < ApplicationController
+  def index
+  end
+
+  def new
+    # @application = Appl.new;
+  end
+end
--- HelloWorld/config/routes.rb
+++ HelloWorld/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'student/index'
+  get 'student/apply'
+  root 'student#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
