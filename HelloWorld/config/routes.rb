e23fb07a79654eab7fea91a4f05e67ea61ed1360
andrew <hunkywei@gmail.com>
2017-10-06 17:55:34 -0500

    lalala

--- HelloWorld/config/routes.rb
+++ HelloWorld/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'student/index'
   get 'student/apply'
   get 'student/checkStatus'
+
+  resources :students
+
   root 'student#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
