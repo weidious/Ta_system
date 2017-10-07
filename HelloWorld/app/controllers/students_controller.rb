1a5b6210e0aee278c72a414225e1502092b40081
andrew <hunkywei@gmail.com>
2017-10-06 20:15:03 -0500

    show

--- HelloWorld/app/controllers/students_controller.rb
+++ HelloWorld/app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
 
   def index
-
+    @students = Student.all
   end
 
   def new
@@ -17,6 +17,16 @@ class StudentsController < ApplicationController
   end
 
   def create
-    render plain: params[:student].inspect
+    #render plain: params[:student].inspect
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to @student
   end
+  def show
+    @student=Student.find(params[:id])
+  end
+private
+    def student_params
+      params.require(:student).permit(:name, :semester)
+    end
 end
