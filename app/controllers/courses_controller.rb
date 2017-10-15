113ff3a8944fdb63a7591df6729a8b57e6f1075e
Zhipei Yan <yanzhipei@hotmail.com>
2017-10-15 20:46:39 +0000

    Added course controller and some views.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,26 @@
 class CoursesController < ApplicationController
+
+    def index
+        @courses=Course.all
+    end
+    
+    
+    def new
+    
+    end
+    
+    
+    def show
+        @course=Course.find(params[:id])
+    end
+    
+    def create
+    
+    end
+
+
+    def destroy
+        
+    end
+    
 end
