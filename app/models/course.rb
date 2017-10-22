ee88bbe2ec445ace2ddf37dea502fbd9881b1506
Bowen Cai <bowen.cai@tamu.edu>
2017-10-22 22:42:06 +0000

    restore course validate rspec

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :instructor, optional: true
   has_many :applies
+  validates :title, presence: true, length: { minimum: 3, maximum: 127}
+  validates :num, presence: true
+  validates_inclusion_of :num, in: 100..999
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  
+  before :each do
+       @course = Course.new(subject:"CSCE", num:606, section:600, title:"Software Engineering")
+   end
+   
+   it "should be valid" do
+ 	  expect(@course).to be_valid
+  end
+   
+   it "should have non-blank coutse title" do
+       @course.title = ""
+       expect(@course).not_to be_valid
+   end
+   
+   it "should have valid course number" do
+       @course.num = 95
+       expect(@course).not_to be_valid
+   end
+ 
 end
