a761a66d0ef1ab079b963834e1e48b53eaf9d6e6
Bowen Cai <bowen.cai@tamu.edu>
2017-10-22 23:54:46 +0000

    delete course cascade offer apply

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :instructor, optional: true
-  has_many :applies
+  has_many :applies, :dependent => :delete_all
+  has_many :offers, :dependent => :delete_all
   validates :title, presence: true, length: { minimum: 3, maximum: 127}
   validates :num, presence: true
   validates_inclusion_of :num, in: 100..999
