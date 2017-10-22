b842485aa22f7d8ed993adaa79dc5b9752f8b514
Bowen Cai <bowen.cai@tamu.edu>
2017-10-22 23:11:36 +0000

    when delete instructor, set nil in course

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,5 @@
 class Instructor < ApplicationRecord
-    has_many :course
+    has_many :course, :dependent => :nullify
     validates :first_name, presence: true, length: { minimum: 3, maximum: 127}
     validates :last_name, presence: true, length: { minimum: 3, maximum: 127}
     validates :email, presence: true, length: { minimum: 3, maximum: 127}, :email_format => { :message => 'is not looking good' }
