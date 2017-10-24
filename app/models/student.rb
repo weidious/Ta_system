7fb3590ce4a6b7a7764bb74de859bc1e71c72fe6
Bowen Cai (蔡博文) <bowen.cai@tamu.edu>
2017-10-23 19:48:30 -0500

    restore validation for student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ApplicationRecord
     has_one:offer
     has_many:applies
+    validates :uin, presence: true
+    validates_inclusion_of :uin, in: 100000000..999999999, :uniqueness => true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, length: {maximum: 31}
+    validates_email_format_of :email, :message => 'email address is not looking good'
 end
