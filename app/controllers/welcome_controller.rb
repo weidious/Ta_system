908267a70fc2df775a6566aae81794b2e2063c04
taobupt <wangtaoo665@gmail.com>
2017-10-29 20:14:31 -0500

    reorganize system

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,15 @@ class WelcomeController < ApplicationController
 
   def login
     #verify the user first in the next step
-    @UIN = params[:student][:UIN]
-    redirect_to controller: 'students', action: 'index', UIN: @UIN
+    session[:student_uin] = params[:uin]
+    session[:user_type] = params[:usertype]
+    if session[:user_type] == "Student"
+      redirect_to controller: 'students', action: 'index'
+    elsif session[:user_type] == "Admin"
+      redirect_to controller: 'courses', action: 'index'
+    elsif session[:user_type] == "Instructor"
+    end
+    # @UIN = params[:student][:UIN]
+
   end
 end
