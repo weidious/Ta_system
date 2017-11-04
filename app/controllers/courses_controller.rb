fb16c2550b8aa3ee96212fb837e3177940b18970
Bowen Cai <bowen.cai@tamu.edu>
2017-11-04 04:14:14 +0000

    course ui update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -27,13 +27,13 @@ class CoursesController < ApplicationController
         @course = Course.find(params[:id])
         @instructors = Instructor.all
         
-      # @ta_candidates = find_candidate_ta(@course)
-      # @grader_candidates = find_candidate_grader(@course)
-      # @sgrader_candidates = find_candidate_sgrader(@course)
-      # puts("==========>>>")
-      # puts(@ta_candidates.length)
-      # puts(@grader_candidates.length)
-      # puts(@sgrader_candidates.length)
+      @ta_candidates = find_candidate_ta(@course)
+      @grader_candidates = find_candidate_grader(@course)
+      @sgrader_candidates = find_candidate_sgrader(@course)
+      puts("==========>>>")
+      puts(@ta_candidates.length)
+      puts(@grader_candidates.length)
+      puts(@sgrader_candidates.length)
       
       # @grader_candidate = []
       # @sgrader_candidates = []
@@ -41,8 +41,6 @@ class CoursesController < ApplicationController
         #@candidates = (Apply.where(course_id: @course.id).collect {|apply| apply.student }).uniq
         @candidates = @course.students.uniq
         @students = Student.all
-        
-        
     end
     
     
@@ -84,113 +82,112 @@ class CoursesController < ApplicationController
         params.require(:course).permit(:subject, :num, :section, :title, :instructor_id, :num_ta, :num_grader, :num_sgrader, :requirement_ta, :requirement_grader, :requirement_sgrader, :ta_candidate_1, :ta_candidate_2, :ta_candidate_3, :ta_candidate_4, :ta_candidate_5, :grader_candidate_1, :grader_candidate_2, :grader_candidate_3, :grader_candidate_4, :grader_candidate_5, :sgrader_candidate_1, :sgrader_candidate_2, :sgrader_candidate_3, :sgrader_candidate_4, :sgrader_candidate_5)
       end
   
-  # def find_candidate_ta(course) 
-  #   ta_candidates = []
-  #   # ta_applies = {}
-  #   if (course.ta_candidate_1) then
-  #     ta1 = Student.find_by_id(@course.ta_candidate_1)
-  #     if (ta1) then
-  #       ta_candidates.push(ta1)
-  #       # apply = Apply.find(student:ta1, course:course, appType:1)
-  #       # ta_applies[ta1.id] = apply
-  #     end
-  #   end
-  #   if (course.ta_candidate_2) then
-  #     ta1 = Student.find_by_id(@course.ta_candidate_2)
-  #     if (ta1) then
-  #       ta_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.ta_candidate_3) then
-  #     ta1 = Student.find_by_id(@course.ta_candidate_3)
-  #     if (ta1) then
-  #       ta_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.ta_candidate_4) then
-  #     ta1 = Student.find_by_id(@course.ta_candidate_4)
-  #     if (ta1) then
-  #       ta_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.ta_candidate_5) then
-  #     ta1 = Student.find_by_id(@course.ta_candidate_5)
-  #     if (ta1) then
-  #       ta_candidates.push(ta1)
-  #     end
-  #   end
-  #   ta_candidates
-  # end
+  def find_candidate_ta(course) 
+    ta_candidates = []
+    # ta_applies = {}
+    if (course.ta_candidate_1) then
+      ta1 = Student.find_by_id(@course.ta_candidate_1)
+      if (ta1) then
+        ta_candidates.push(ta1)
+        # apply = Apply.find(student:ta1, course:course, appType:1)
+        # ta_applies[ta1.id] = apply
+      end
+    end
+    if (course.ta_candidate_2) then
+      ta1 = Student.find_by_id(@course.ta_candidate_2)
+      if (ta1) then
+        ta_candidates.push(ta1)
+      end
+    end
+    if (course.ta_candidate_3) then
+      ta1 = Student.find_by_id(@course.ta_candidate_3)
+      if (ta1) then
+        ta_candidates.push(ta1)
+      end
+    end
+    if (course.ta_candidate_4) then
+      ta1 = Student.find_by_id(@course.ta_candidate_4)
+      if (ta1) then
+        ta_candidates.push(ta1)
+      end
+    end
+    if (course.ta_candidate_5) then
+      ta1 = Student.find_by_id(@course.ta_candidate_5)
+      if (ta1) then
+        ta_candidates.push(ta1)
+      end
+    end
+    ta_candidates
+  end
   
-  # def find_candidate_grader(course)
-  #   grader_candidates = []
-  #   if (course.grader_candidate_1) then
-  #     ta1 = Student.find_by_id(@course.grader_candidate_1)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.grader_candidate_2) then
-  #     ta1 = Student.find_by_id(@course.grader_candidate_2)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.grader_candidate_3) then
-  #     ta1 = Student.find_by_id(@course.grader_candidate_3)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.grader_candidate_4) then
-  #     ta1 = Student.find_by_id(@course.grader_candidate_4)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.grader_candidate_5) then
-  #     ta1 = Student.find_by_id(@course.grader_candidate_5)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   grader_candidates
-  # end
+  def find_candidate_grader(course)
+    grader_candidates = []
+    if (course.grader_candidate_1) then
+      ta1 = Student.find_by_id(@course.grader_candidate_1)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.grader_candidate_2) then
+      ta1 = Student.find_by_id(@course.grader_candidate_2)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.grader_candidate_3) then
+      ta1 = Student.find_by_id(@course.grader_candidate_3)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.grader_candidate_4) then
+      ta1 = Student.find_by_id(@course.grader_candidate_4)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.grader_candidate_5) then
+      ta1 = Student.find_by_id(@course.grader_candidate_5)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    grader_candidates
+  end
   
-  
-  # def find_candidate_sgrader(course)
-  #   grader_candidates = []
-  #   if (course.sgrader_candidate_1) then
-  #     ta1 = Student.find_by_id(@course.sgrader_candidate_1)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.sgrader_candidate_2) then
-  #     ta1 = Student.find_by_id(@course.sgrader_candidate_2)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.sgrader_candidate_3) then
-  #     ta1 = Student.find_by_id(@course.sgrader_candidate_3)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.sgrader_candidate_4) then
-  #     ta1 = Student.find_by_id(@course.sgrader_candidate_4)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   if (course.sgrader_candidate_5) then
-  #     ta1 = Student.find_by_id(@course.sgrader_candidate_5)
-  #     if (ta1) then
-  #       grader_candidates.push(ta1)
-  #     end
-  #   end
-  #   grader_candidates
-  # end
+  def find_candidate_sgrader(course)
+    grader_candidates = []
+    if (course.sgrader_candidate_1) then
+      ta1 = Student.find_by_id(@course.sgrader_candidate_1)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.sgrader_candidate_2) then
+      ta1 = Student.find_by_id(@course.sgrader_candidate_2)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.sgrader_candidate_3) then
+      ta1 = Student.find_by_id(@course.sgrader_candidate_3)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.sgrader_candidate_4) then
+      ta1 = Student.find_by_id(@course.sgrader_candidate_4)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    if (course.sgrader_candidate_5) then
+      ta1 = Student.find_by_id(@course.sgrader_candidate_5)
+      if (ta1) then
+        grader_candidates.push(ta1)
+      end
+    end
+    grader_candidates
+  end
   
 end
