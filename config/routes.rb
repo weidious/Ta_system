8058735d75e0020dd0c4e93fa70bc4b0154c48b6
Zhipei Yan <yanzhipei@hotmail.com>
2017-11-04 03:15:18 +0000

    Added admin untracked files.

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   end
 
   resources :instructors
-  resources :courses
+  resources :courses do
+    resources :offers
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
