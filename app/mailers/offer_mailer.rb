e8a90969f64c11343a4eb7546f9624927ef17d10
taobupt <wangtaoo665@gmail.com>
2017-11-05 22:00:29 -0600

    add mailer

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -2,8 +2,8 @@ class OfferMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
   def welcome_email(offer)
-    @user = offer.student
+    @student = offer.student
     @url  = 'http://example.com/login'
-    mail(to: "taobupt@tamu.edu", subject: 'Welcome to My Awesome Site')
+    mail(to: @student.email, subject: 'Welcome to My Awesome Site')
   end
 end
