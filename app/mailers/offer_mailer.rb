1a7d0d897816a0ab1ba0e5444f1b451d3f714a59
taobupt <taobupt@tamu.edu>
2017-11-28 10:47:51 -0600

    change the website address

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -11,7 +11,7 @@ class OfferMailer < ApplicationMailer
     @student = offer.student
     @encryptNetID = encrypt('taobupt')
     @encryptPosition = encrypt('grader')
-    @url  = 'http://localhost:3000/offer_decision/decide?netId=%s&position=%s' % [@encryptNetID,@encryptPosition]
+    @url  = 'https://pacific-wildwood-55984.herokuapp.com'
     mail(to: @student.email, subject: 'Welcome to TAMU TA Application System')
   end
 end
