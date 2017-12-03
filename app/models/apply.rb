22a4633ca35b69f2a2eee8951c60113d3f514eab
Bowen Cai <bowen.cai@tamu.edu>
2017-12-03 00:01:16 +0000

    add more model rspec

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -6,34 +6,8 @@ class Apply < ApplicationRecord
   validates :appType, presence: true
   validates :positive, inclusion: { in: [true, false] }
   validates :acceptAdjust, inclusion: { in: [true, false] }
-  #validates :acceptAdjust, exclusion: { in: [nil] }
   validates :takenBefore, inclusion: { in: [true, false] }
   
   enum appType: [ :ta, :senior_grader, :grader ]
-  
-  def self.to_csv
-    attributes = %w{id email name}
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-      all.each do |user|
-        csv << attributes.map{ |attr| user.send(attr) }
-      end
-    end
-  end
-  
-  # def self.appTypeEnum(i)
-  #   case i
-  #   when 1
-  #     'TA'
-  #   when 2
-  #     'Senior Grader'
-  #   else
-  #     'Grader'
-  #   end
-  # end
-  
-  # def appTypeCaption
-  #   Apply.appTypeEnum(appType)
-  # end
   
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  
-  
+
   before :each do
        @course = Course.new(subject:"CSCE", num:606, section:600, title:"Software Engineering")
    end
@@ -12,8 +10,13 @@ RSpec.describe Course, type: :model do
  	  expect(@course).to be_valid
   end
    
-   it "should have non-blank coutse title" do
+   it "should have non-blank course title" do
        @course.title = ""
+       expect(@course).not_to be_valid
+   end
+   
+   it "should have course number" do
+       @course.num = nil
        expect(@course).not_to be_valid
    end
    
@@ -21,5 +24,41 @@ RSpec.describe Course, type: :model do
        @course.num = 95
        expect(@course).not_to be_valid
    end
+   
+   it "should have caption" do
+       expect(@course.caption).to eql("CSCE 606-600  Software Engineering")
+   end
+   
+   it "should have ta_candidate_ids" do
+       expect(@course.ta_candidate_ids).to eql([])
+   end
  
+   it "should have grader_candidate_ids" do
+       expect(@course.grader_candidate_ids).to eql([])
+   end
+   
+   it "should have sgrader_candidate_ids" do
+       expect(@course.sgrader_candidate_ids).to eql([])
+   end
+     
+   it "should generate_offers" do
+       @course.save!
+       expect(@course.generate_offers).to eql(0)
+   end
+   
+   it "should be able to update_ta_candidates" do
+       @course.update_ta_candidates([1,2,3,4,5])
+       expect(@course.ta_candidate_ids).to eql([1,2,3,4,5])
+   end
+   
+   it "should be able to update_grader_candidates" do
+       @course.update_grader_candidates([5,4,3,2,1])
+       expect(@course.grader_candidate_ids).to eql([5,4,3,2,1])
+   end
+   
+   it "should be able to update_sgrader_candidates" do
+       @course.update_sgrader_candidates([1,2,3,4,5])
+       expect(@course.sgrader_candidate_ids).to eql([1,2,3,4,5])
+   end
+   
 end
